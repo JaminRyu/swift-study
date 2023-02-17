1fd92e79c9442db6b3934e293cf65c7b8dcbe400
kamei <65549294+karax233@users.noreply.github.com>
2023-02-17 20:51:01 +0900

    Add files via upload

---
+++ EmojiArt/EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Jiaming Liu on 2023/01/06.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
